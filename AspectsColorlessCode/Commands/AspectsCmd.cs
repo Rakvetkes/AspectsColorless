fd0400c1462a434ff87ecf9131321bea54cac805
Mataura <3121043930@qq.com>
2026-06-30 00:45:45 +0800

    the log

--- AspectsColorlessCode/Commands/AspectsCmd.cs
+++ AspectsColorlessCode/Commands/AspectsCmd.cs
@@ -1,7 +1,6 @@
 ﻿using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Players;
-using MegaCrit.Sts2.Core.Factories;
 using MegaCrit.Sts2.Core.Models;
 
 namespace AspectsColorless.AspectsColorlessCode.Commands;
@@ -11,21 +10,14 @@ public static class AspectsCmd
     public static async Task<IEnumerable<CardPileAddResult?>> Transmute(CardModel card, Player performer)
     {
         var result = new List<CardPileAddResult?>();
-        var replacement = CardFactory.GetDistinctForCombat(performer,
-            performer.Character.CardPool.GetUnlockedCards(performer.UnlockState, performer.RunState.CardMultiplayerConstraint),
-            1, performer.RunState.Rng.CombatCardGeneration).First();
-        
         var deckVersion = card.DeckVersion;
-        result.Add(await CardCmd.Transform(card, replacement));
+        result.Add(await CardCmd.TransformToRandom(card, performer.RunState.Rng.CombatCardSelection));
         if (deckVersion is { IsTransformable: true })
         {
-            var transformed = result.First();
-            var deckReplacement = deckVersion.Owner.RunState.CloneCard(replacement);
+            var transformed = result.First()!.Value.cardAdded;
+            var deckReplacement = deckVersion.Owner.RunState.CloneCard(transformed);
             result.Add(await CardCmd.Transform(deckVersion, deckReplacement));
-            if (transformed != null)
-            {
-                transformed.Value.cardAdded.DeckVersion = deckReplacement;
-            }
+            transformed.DeckVersion = deckReplacement;
         }
         
         return result;
